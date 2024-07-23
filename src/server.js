2b7557d1e74fb65c4efb4daf6015b59b20162be9
Heber Deleba <delebauh@gmail.com>
2024-07-23 16:49:11 -0300

    primera pre-entrega backend2

--- src/server.js
+++ src/server.js
@@ -4,24 +4,50 @@ import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 import { errorHandler } from './middlewares/error.Handler.js';
 import { initMongoDB } from './db/database.js';
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { initializePassport } from "./config/passport.config.js";
+
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to the home page!');
+});
 
+
+
+// configuracion de express
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+// configuracion de passport
+initializePassport();
+app.use(passport.initialize());
 
 
+
+// Rutas
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use('/products', productRouter);
 app.use('/carts', cartRouter);
 
 app.use(errorHandler);
 
-initMongoDB();
 
+
+//inicializador mongo
+
+initMongoDB()
 const PORT = 8080;
 
-app.listen(PORT, () => console.log(`SERVER ON PORT ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  });
 
 
 
